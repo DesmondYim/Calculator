5e6eac52c5de2fe499e9443cb17e0af0848d7ed7
Desmond Yim <desmond.y01@gmail.com>
2022-02-23 08:36:37 -0700

    create UI

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    // store first num when operator clicked
+    let firstNum = document.querySelector(#userInput)
+    // store operator when clicked
+    // document.querySelector(".operate") => for operator buttons
+    // document.querySelector(element.HtmlElement) => for individual operators
+
+    let operatorBtn = document.querySelector(".operatorBtn"); 
+    operatorBtn.addEventListener("click", () => {
+        return operator = 
+    })
+    // create operate function
+    function operate() {
+        return Number(firstNum) operator
+    }
+    // create result from operation
+
+
+}
